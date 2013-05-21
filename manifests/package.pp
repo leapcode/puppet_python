87332b4032cac4c2f0fc33e55514b81fc9f00645
Micah Anderson <micah@riseup.net>
2013-05-21 17:02:41 -0400

    add a python::package define

---
+++ manifests/package.pp
@@ -0,0 +1,13 @@
+define python::package ( $ensure = 'present' )
+{
+  if $ensure == 'present' { require python }
+
+  case $operatingsystem {
+    'debian','ubuntu': {
+        package {"python-${name}": ensure => $ensure }
+    }
+    default: {
+      package {"python-${name}": ensure => $ensure }
+    }
+  }
+}
